b616b325f4fb43029f2fbe0ba8d583cc3caa7f85
Robert Cheseldine <robert.cheseldine@gmail.com>
2020-02-03 23:00:35 -0500

    updated global layout with background

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,8 @@ const Layout = props => (
     <div>
         <Header />
         {props.children}
+        <div className="bg__"></div>
+
         <style jsx global>{`
             @import url("https://fonts.googleapis.com/css?family=Muli:300,400,600&display=swap");
 
@@ -14,6 +16,27 @@ const Layout = props => (
                  {
                     /* background: #1f1f1f; */
                 }
+            }
+
+            .bg__ {
+                width: 110vw;
+                position: fixed;
+                color: #ff43ea;
+                z-index: -100;
+            }
+
+            .bg__::before {
+                content: "";
+                top: -50px;
+                left: -10px;
+                width: 110%;
+                height: 110%;
+                position: fixed;
+                box-shadow: inset 0px 20px 150px 75px #212121;
+                background-image: url("static/anders-krogh-jorgensen-sYY1FlMHOV4-unsplash.jpg");
+                background-size: cover;
+                filter: sepia(50%) saturate(2) grayscale(90%);
+                z-index: -100;
             }
         `}</style>
     </div>
